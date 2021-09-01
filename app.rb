23795588c33173b9bebd762b101da3992da5f834
Dev <dwldviljoen@gmail.com>
2021-09-01 16:16:50 +0100

    new quotes can be created

--- app.rb
+++ app.rb
@@ -16,5 +16,19 @@ class ProjectOrg < Sinatra::Base
     erb:'quotes/index'
   end
 
+  get '/quotes/new' do
+    erb:"quotes/new"
+  end
+
+  post '/quotes' do
+    Quotes.create(date_created: params['date_created'],
+                  client_id: params['client_id'],
+                  project_scope: params['project_scope'],
+                  materials_table: params['materials_table'],
+                  labour_table: params['labour_table'],
+                  expenses_table: params['expenses_table'])
+    redirect '/quotes'
+  end
+
   run! if app_file == $0
 end
--- lib/quotes.rb
+++ lib/quotes.rb
@@ -11,4 +11,15 @@ class Quotes
     result = @connection.execute("SELECT * FROM quotes;")
   end
 
+  def self.create(date_created:, client_id:, project_scope:,
+    materials_table:, labour_table:, expenses_table: )
+    if ENV['ENVIRONMENT'] == 'test'
+      @connection = SQLite3::Database.new "project_org_test.db"
+    else  
+      @connection = SQLite3::Database.new "project_org.db"  
+    end
+    @connection.execute("INSERT INTO quotes (date_created, client_id, project_scope, materials_table, labour_table, expenses_table) VALUES ('#{date_created}', '#{client_id}', '#{project_scope}', '#{materials_table}', '#{labour_table}', '#{expenses_table}');")
+    
+  end
+
 end
